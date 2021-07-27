c21ad1573a5eeaaa07e12617b6cac0242b8fd9a4
odupree1988 <odupree1988@gmail.com>
2021-07-27 14:52:49 -0400

    heroku

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+  },
+  { sequelize, freezeTableName: true, underscored: true, modelName: "post" }
+);
