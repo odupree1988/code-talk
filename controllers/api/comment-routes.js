32753a1ce329f12f08a41b4571d5f48e59933149
odupree1988 <odupree1988@gmail.com>
2021-07-28 16:36:16 -0400

    comment routes added

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,24 +1,65 @@
 const router = require("express").Router();
+const { json } = require("sequelize/types");
 const { User, Post, Comment } = require("../../models");
 
 router.get("/", (req, res) => {
-  Comment.findAll();
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/:id", (req, res) => {
-  Comment.findOne();
+  Comment.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment exists with this id!" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
-  Comment.create();
-});
-
-router.put("/:id", (req, res) => {
-  Comment.update();
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbCommentData) => json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
-  Comment.destroy();
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment exists with this id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
